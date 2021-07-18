397bbf972c1de516cafa8e589017cff581ffbb44
emintatli <asasautorun12@gmail.com>
2021-07-18 22:55:06 +0300

    new project add

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -118,7 +118,20 @@ chatting with SocketIO,saving all chatting data on MongoDB</i></span>
 
     <div className="card my-2">
         <div className="card-body d-flex align-items-center justify-content-between back-front mobile-projects">
-            <span className="text-dark w-50 me-3"><b><span className="alert alert-success sort-alert">#10</span> React Book Search App </b></span>
+            <span className="text-dark w-50 me-3"><b><span className="alert alert-success sort-alert">#10</span> Rock, Paper ,Scissors React Game </b></span>
+            <span className="text-dark"></span>
+            <div className="d-flex mobile-projects">
+            <div className="alert alert-primary mx-1"><span className="no-wrap"> Live Preview : <a href="https://rock-paper-scissors-app-ten.vercel.app/"><b>DEMO</b></a>
+            </span></div>
+            <div className="alert alert-warning mx-1 no-wrap"><span> Source Code : <a href="https://github.com/emintatli/rock-paper-scissors-app"><b>Github</b></a>
+            </span></div>
+            </div>
+        </div>
+    </div>
+
+    <div className="card my-2">
+        <div className="card-body d-flex align-items-center justify-content-between back-front mobile-projects">
+            <span className="text-dark w-50 me-3"><b><span className="alert alert-success sort-alert">#11</span> React Book Search App </b></span>
             <span className="text-dark"><i><b>Features</b>:Google Books Rest API</i></span>
             <div className="d-flex mobile-projects">
             <div className="alert alert-primary mx-1"><span className="no-wrap"> Live Preview : <a href="https://book-search-app.vercel.app/"><b>DEMO</b></a>
@@ -131,7 +144,7 @@ chatting with SocketIO,saving all chatting data on MongoDB</i></span>
 
     <div className="card my-2">
         <div className="card-body d-flex align-items-center justify-content-between back-front mobile-projects">
-            <span className="text-dark w-50 me-3"><b><span className="alert alert-success sort-alert">#11</span> BMI Calculator </b></span>
+            <span className="text-dark w-50 me-3"><b><span className="alert alert-success sort-alert">#12</span> BMI Calculator </b></span>
             <span className="text-dark"><i><b>Features</b>:React Router</i></span>
             <div className="d-flex mobile-projects">
             <div className="alert alert-primary mx-1"><span className="no-wrap"> Live Preview : <a href="https://bmi-index-calc.vercel.app/"><b>DEMO</b></a>
@@ -144,7 +157,7 @@ chatting with SocketIO,saving all chatting data on MongoDB</i></span>
 
     <div className="card my-2">
         <div className="card-body d-flex align-items-center justify-content-between back-front mobile-projects">
-            <span className="text-dark w-50 me-3"><b><span className="alert alert-success sort-alert">#12</span> Pokemon Match Game </b></span>
+            <span className="text-dark w-50 me-3"><b><span className="alert alert-success sort-alert">#13</span> Pokemon Match Game </b></span>
             <span className="text-dark"><i></i></span>
             <div className="d-flex mobile-projects">
             <div className="alert alert-primary mx-1"><span className="no-wrap"> Live Preview : <a href="https://flip-card-game-gamma.vercel.app/"><b>DEMO</b></a>
@@ -157,7 +170,7 @@ chatting with SocketIO,saving all chatting data on MongoDB</i></span>
     
     <div className="card my-2">
         <div className="card-body d-flex align-items-center justify-content-between back-other mobile-projects">
-            <span className="text-dark w-50 me-3"><b><span className="alert alert-info sort-alert">#13</span> Face Recognition Payment System</b></span>
+            <span className="text-dark w-50 me-3"><b><span className="alert alert-info sort-alert">#14</span> Face Recognition Payment System</b></span>
             <span className="text-dark"><i><b>Features</b>:Face detection,OpenCV,Numpy Array DB,Socket</i></span>
             <div className="d-flex mobile-projects">
             <div className="alert alert-danger mx-1"><span className="no-wrap"> Live Preview : <s>DEMO</s>
@@ -170,7 +183,7 @@ chatting with SocketIO,saving all chatting data on MongoDB</i></span>
 
     <div className="card my-2">
         <div className="card-body d-flex align-items-center justify-content-between back-other mobile-projects">
-            <span className="text-dark w-50 me-3"><b><span className="alert alert-info sort-alert">#14</span> Detecting Electronic Card Issues</b></span>
+            <span className="text-dark w-50 me-3"><b><span className="alert alert-info sort-alert">#15</span> Detecting Electronic Card Issues</b></span>
             <span className="text-dark"><i><b>Features</b>:Image processing with matrix,MATLAB project</i></span>
             <div className="d-flex mobile-projects">
             <div className="alert alert-danger mx-1"><span className="no-wrap"> Live Preview : <s>DEMO</s>
