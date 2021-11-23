2adea84f5c0704f570d9b57fb6d8a8787ab30e10
emintatli <asasautorun12@gmail.com>
2021-11-23 18:26:34 +0300

    next fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,7 +21,7 @@ return <>
 alt="html5" width="40" height="40"/></a> 
 <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> 
 <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/></a>
-<a href="https://nextjs.org/" target="_blank"><img src="https://cdn.worldvectorlogo.com/logos/nextjs-3.svg" alt="nextjs" width="40" height="40"/></a> 
+<a href="https://nextjs.org/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/800px-Nextjs-logo.svg.png" alt="nextjs" width="60" height="40"/></a> 
 <a href="https://angular.io/" target="_blank"> 
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png" alt="angular" width="40" height="40"/></a>
        
